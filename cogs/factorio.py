ed39e8fcf26f55a4bcaf758e1ed84da228fe07e0
arielbeje <arielb624@gmail.com>
2018-01-30 13:58:01 +0200

    Added anime command.

--- cogs/factorio.py
+++ cogs/factorio.py
@@ -1,11 +1,10 @@
 from ago import human
 import aiohttp
-import datetime
 import bs4
+import datetime
 import json
 import tomd
 import re
-import requests
 
 import discord
 from discord.ext import commands
@@ -23,17 +22,15 @@ def mod_embed(r, bot, modlink):
     fields.append({"name": "Owner", "value": r["owner"]})
     for tag in r["tags"]:
         taglist.append(tag["title"])
-    fields.append({"name": "Tags", "value": ', '.join(taglist)})
-    # fields.append({"name": "Version", "value": r["releases"][0]["version"]})
-    fields.append({"name": "Game Version", "value": r["releases"][0]["game_version"]})
-    # fields.append({"name": "License", "value": f"[{r['license_name']}]({r['license_url']})"})
-    fields.append({"name": "Downloads", "value": str(r["downloads_count"])})
+    fields.extend([{"name": "Tags", "value": ', '.join(taglist)},
+                   {"name": "Game Version", "value": r["releases"][0]["game_version"]},
+                   {"name": "Downloads", "value": str(r["downloads_count"])}])
     for item in ["created_at", "updated_at"]:
         r[item] = re.sub(r"(?P<before>[+-]\d{2})(:)(?P<after>\d{2})", r"\g<before>\g<after>", r[item])
         r[item] = datetime.datetime.strptime(r[item], "%Y-%m-%d %H:%M:%S.%f%z")
         r[item] = human(r[item], precision=1)
-    fields.append({"name": "Released", "value": r["created_at"]})
-    fields.append({"name": "Updated", "value": r["updated_at"]})
+    fields.extend([{"name": "Released", "value": r["created_at"]},
+                   {"name": "Updated", "value": r["updated_at"]}])
     for field in fields:
         em.add_field(**field, inline=True)
     # em.set_footer(text=bot.user.name, icon_url=f"https://cdn.discordapp.com/avatars/{bot.user.id}/{bot.user.avatar}.png?size=64")
@@ -100,40 +97,48 @@ class FactorioCog():
         """
         Searches for a term in the [official Factorio wiki](https://wiki.factorio.com/).
         """
-        r = requests.get(f"https://wiki.factorio.com/index.php?search={searchterm.title().replace(' ', '%20')}")
-        soup = bs4.BeautifulSoup(r.text, 'html.parser')
-        if soup.find('p', class_='mw-search-nonefound'):
-            em = discord.Embed(title="Error",
-                               description=f"Could not find \"{searchterm.title()}\" in wiki.",
-                               colour=0xDC143C)
-            # em.set_footer(text=self.bot.user.name, icon_url=f"https://cdn.discordapp.com/avatars/{self.bot.user.id}/{self.bot.user.avatar}.png?size=64")
-            await ctx.send(embed=em)
-            return
-        if soup.find_all('ul', class_="mw-search-results"):
-            em = discord.Embed(title="Factorio Wiki",
-                               url=r.url,
-                               colour=0xDFDE6E)
-            for item in soup.find_all('ul', class_="mw-search-results")[0].find_all("li"):
-                item = item.find_next('div', class_="mw-search-result-heading").find('a')
-                itemlink = item['href'] if not item['href'].endswith(")") else item['href'].replace(")", "\)")
-                em.add_field(name=item['title'], value=f"[Read More](https://wiki.factorio.com{itemlink})", inline=True)
-            # em.set_footer(text=self.bot.user.name, icon_url=f"https://cdn.discordapp.com/avatars/{self.bot.user.id}/{self.bot.user.avatar}.png?size=64")
-            await ctx.send(embed=em)
-        else:
-            description_ = ""
-            if soup.select("#mw-content-text > p"):
-                pNum = 0
-                if re.match(r"this (article|page)", str(soup.select("#mw-content-text > p")[0])):
-                    pNum = 1
-                description_ = tomd.convert(str(soup.select("#mw-content-text > p")[pNum])).strip()
-            em = discord.Embed(title=soup.find("h1", id='firstHeading').get_text(),
-                               description=re.sub(r"\((\/\S*)\)", r"(https://wiki.factorio.com\1)", description_),
-                               url=r.url,
-                               colour=0x19B300)
-            if soup.find('div', class_="factorio-icon"):
-                em.set_thumbnail(url=f"https://wiki.factorio.com{soup.find('div', class_='factorio-icon').find('img')['src']}")
-            # em.set_footer(text=self.bot.user.name, icon_url=f"https://cdn.discordapp.com/avatars/{self.bot.user.id}/{self.bot.user.avatar}.png?size=64")
-            await ctx.send(embed=em)
+        em = discord.Embed(title=f"Searching for \"{searchterm.title()}\" in wiki.factorio.com...",
+                           description="This shouldn't take long.",
+                           colour=0xDFDE6E)
+        # em.set_footer(text=self.bot.user.name, icon_url=f"https://cdn.discordapp.com/avatars/{self.bot.user.id}/{self.bot.user.avatar}.png?size=64")
+        bufferMsg = await ctx.send(embed=em)
+        async with ctx.channel.typing():
+            async with aiohttp.ClientSession() as client:
+                async with client.get(f"https://wiki.factorio.com/index.php?search={searchterm.replace(' ', '%20')}") as resp:
+                    r = await resp.text()
+            soup = bs4.BeautifulSoup(r.text, 'html.parser')
+            if soup.find('p', class_='mw-search-nonefound'):
+                em = discord.Embed(title="Error",
+                                   description=f"Could not find \"{searchterm.title()}\" in wiki.",
+                                   colour=0xDC143C)
+                # em.set_footer(text=self.bot.user.name, icon_url=f"https://cdn.discordapp.com/avatars/{self.bot.user.id}/{self.bot.user.avatar}.png?size=64")
+                await bufferMsg.edit(embed=em)
+                return
+            if soup.find_all('ul', class_="mw-search-results"):
+                em = discord.Embed(title="Factorio Wiki",
+                                   url=r.url,
+                                   colour=0xDFDE6E)
+                for item in soup.find_all('ul', class_="mw-search-results")[0].find_all("li"):
+                    item = item.find_next('div', class_="mw-search-result-heading").find('a')
+                    itemlink = item['href'] if not item['href'].endswith(")") else item['href'].replace(")", "\)")
+                    em.add_field(name=item['title'], value=f"[Read More](https://wiki.factorio.com{itemlink})", inline=True)
+                # em.set_footer(text=self.bot.user.name, icon_url=f"https://cdn.discordapp.com/avatars/{self.bot.user.id}/{self.bot.user.avatar}.png?size=64")
+                await bufferMsg.edit(embed=em)
+            else:
+                description_ = ""
+                if soup.select("#mw-content-text > p"):
+                    pNum = 0
+                    if re.search(r"((^<br/>$)|(This (article|page)))", str(soup.select("#mw-content-text > p")[0])):
+                        pNum = 1
+                    description_ = tomd.convert(str(soup.select("#mw-content-text > p")[pNum])).strip().replace("<br/>", "\n")
+                em = discord.Embed(title=soup.find("h1", id='firstHeading').get_text(),
+                                   description=re.sub(r"\((\/\S*)\)", r"(https://wiki.factorio.com\1)", description_),
+                                   url=r.url,
+                                   colour=0x19B300)
+                if soup.find('div', class_="factorio-icon"):
+                    em.set_thumbnail(url=f"https://wiki.factorio.com{soup.find('div', class_='factorio-icon').find('img')['src']}")
+                # em.set_footer(text=self.bot.user.name, icon_url=f"https://cdn.discordapp.com/avatars/{self.bot.user.id}/{self.bot.user.avatar}.png?size=64")
+                await bufferMsg.edit(embed=em)
 
 
 def setup(bot):
--- cogs/userutils.py
+++ cogs/userutils.py
@@ -1,7 +1,11 @@
+from ago import human
+import aiohttp
 import asyncio
 import datetime
 import re
 import json
+import pytz
+import xml.etree.ElementTree as ET
 
 import discord
 from discord.ext import commands
@@ -9,6 +13,9 @@ from discord.ext import commands
 
 with open('data/reminderdb.json', 'r') as f:
     reminderdb = json.load(f)
+
+with open('variables.json') as f:
+    variables = json.load(f)
 
 
 async def reminder_check(bot, reminderdb=reminderdb):
@@ -87,23 +94,134 @@ class UserUtils:
             em.description = f"You will be reminded in {', '.join(timeString[:-1])} and {timeString[-1]} from now."
         # em.set_footer(text=self.bot.user.name, icon_url=f"https://cdn.discordapp.com/avatars/{self.bot.user.id}/{self.bot.user.avatar}.png?size=64")
         await ctx.send(embed=em)
-    
+
     @commands.command(name='userinfo')
-    async def user_info(self, ctx, user=discord.User=None):
+    async def user_info(self, ctx, user: discord.User=None):
         """Returns information about the given user"""
         if not user:
             user = ctx.message.author
-        
-        name = user.name
-        
-        if user.display_name != user.name:
-            nickname = user.display_name
+        member = ctx.message.guild.get_member(user.id)
+        em = discord.Embed(colour=0xDFDE6E)
+        inlineFields = [
+            {"name": "ID", "value": member.id},
+            {"name": "Nickname", "value": member.nick if not None else "None"},
+            {"name": "Status", "value": member.status},
+            {"name": "Playing", "value": member.game if not None else "None"},
+            {"name": "Mention", "value": member.mention}
+        ]
+        for field in inlineFields:
+            em.add_field(**field, inline=True)
+        avatar = member.avatar_url_as(size=64)  # if not None else discord.Embed.Empty
+        registeredAt = pytz.utc.localize(member.created_at)
+        joinedAt = pytz.utc.localize(member.joined_at)
+        em.add_field(name="Joined", value=f"{human(joinedAt, precision=4)} ({joinedAt.strftime('%d-%m-%Y %H:%M:%S %Z')})")
+        em.add_field(name="Roles", value=", ".join([role.name for role in member.roles]).replace("@everyone", "@\u200beveryone"))
+        em.set_author(name=member.name, icon_url=avatar)
+        em.set_thumbnail(url=avatar)
+        em.set_footer(text=f"Created: {human(registeredAt, precision=4)} ({registeredAt.strftime('%d-%m-%Y %H:%M:%S %Z')})")
+        await ctx.send(embed=em)
 
-        userid = user.id
-        avatar = user.avatar_url
-        registeredAt = user.created_at
-        joinedAt = ''
-        #ariel pls make pretty embed
+    @commands.command(name="anime")
+    async def anime_search(self, ctx, *, query: str=""):
+        """
+        Searches MyAnimeList for given anime and returns some info.
+        """
+        if not query:
+            em = discord.Embed(title="Error",
+                               description="A search term is required.",
+                               colour=0xDC143C)
+            await ctx.send(embed=em)
+            return
+        em = discord.Embed(title=f"Searching for \"{query.title()}\" in myanimelist.net...",
+                           description="This may take a bit.",
+                           colour=0xDFDE6E)
+        # em.set_footer(text=self.bot.user.name, icon_url=f"https://cdn.discordapp.com/avatars/{self.bot.user.id}/{self.bot.user.avatar}.png?size=64")
+        bufferMsg = await ctx.send(embed=em)
+        async with ctx.channel.typing():
+            if variables["mynaimelist"]["login"] and variables["mynaimelist"]["password"]:
+                try:
+                    async with aiohttp.ClientSession(auth=aiohttp.BasicAuth(**variables["mynaimelist"])) as client:
+                        async with client.get(f"https://myanimelist.net/api/anime/search.xml?q={query.replace(' ', '+')}") as resp:
+                            assert resp.status == 200
+                            mal_data = ET.fromstring(await resp.text())[0]
+                        '''async with client.get(f"https://cdn.animenewsnetwork.com/encyclopedia/api.xml?type=anime&title=~{query}") as resp:
+                            data = ET.fromstring(await resp.text())[0][1:]'''
+                except AssertionError:
+                    print("Invalid mynaimelist username/password.")
+                    return
+            else:
+                print("To use the anime command, a myanimelist login has to be inputted in variables.json.")
+                return
+
+            choosingList = []
+            messageToSend = "Please choose the correct show by entering its number.\n\n"
+            i = 1
+            for item in mal_data:
+                if i <= 10:
+                    choosingList.append(item)
+                    messageToSend += f"[{i}] # {item[2].text}\n"
+                    i += 1
+                else:
+                    break
+            messageToSend += ("\n\nType \"exit\" to leave this menu.\n"
+                              "# If no choice is made within 60 seconds, this message will be deleted.")
+            await bufferMsg.edit(content=f"```python\n{messageToSend}```", embed=None)
+
+            def check(message):
+                content = message.content
+                contentIsIndex = False
+                try:
+                    int(content)
+                    contentIsIndex = len(choosingList) >= int(content) - 1 and not int(content) - 1 < 1
+                except ValueError:
+                    pass
+                return message.author == ctx.message.author and contentIsIndex or content == "exit"
+
+            try:
+                msg = await self.bot.wait_for('message', timeout=60.0, check=check)
+                if msg.content == "exit":
+                    await bufferMsg.delete()
+                else:
+                    data = choosingList[int(msg.content) - 1]
+                    # If bot has manage messages perm, delete the message
+                    em = discord.Embed(title=data[2].text,
+                                       url=f"https://myanimelist.net/anime/{data[0].text}",
+                                       description=data[11].replace("<br />", "").replace("&#039;", "'"),
+                                       # TODO: Improve cleaning of synopsis.
+                                       colour=0x19B300)
+                    em.set_thumbnail(url=data[11].text)
+                    if data[2] != data[1]:
+                        em.description = f"**Alternative title:** {data[2].text}"
+                    fields = []
+                    start_date = datetime.datetime.strptime(data[8].text, "%Y-%m-%d")
+                    if data[4].text == "0":
+                        fields.append({"name": "Episodes", "value": "Unknown"})
+                    else:
+                        fields.append({"name": "Episodes", "value": data[4].text})
+                    fields.extend([{"name": "Status", "value": data[7].text},
+                                   {"name": "Start Date", "value": start_date.strftime("%d-%m-%Y")}])
+                    if data[9].text != "0000-00-00":
+                        end_date = datetime.datetime.strptime(data[9].text, "%Y-%m-%d")
+                        fields.append({"name": "End Date", "value": end_date.strftime("%d-%m-%Y")})
+                    if data[5].text != "0.00":
+                        fields.append({"name": "Score", "value": f"{data[5].text}/10"})
+                    for field in fields:
+                        em.add_field(**field, inline=True)
+                    em.set_footer(text="Powered by myanimelist.net")
+
+                    await bufferMsg.edit(embed=em, content=None)
+            except asyncio.TimeoutError:
+                await bufferMsg.delete()
+
+    @anime_search.error
+    async def mal_search_error_handler(self, ctx, error):
+        origerror = getattr(error, 'original', error)
+        if isinstance(origerror, aiohttp.http_exceptions.BadHttpMessage):
+            em = discord.Embed(title="Error",
+                               description="Couldn't find query in [My Anime List](https://myanimelist.net/) DB.",
+                               colour=0xDC143C)
+            await ctx.send(embed=em)
+
 
 def setup(bot):
     bot.loop.create_task(reminder_check(bot))
