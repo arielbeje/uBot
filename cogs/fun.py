e9d119557e21f9dc6052d1b059173f442b91870e
SpeckledFleebeedoo <datema150@gmail.com>
2023-05-14 15:28:01 +0200

    expand expansion command

--- cogs/fun.py
+++ cogs/fun.py
@@ -1,4 +1,5 @@
 import random
+import asyncio
 from discord.ext import commands
 
 class FunCog(commands.Cog):
@@ -14,13 +15,24 @@ class FunCog(commands.Cog):
         """
         rndInt = random.randint(0, 20)
         if rndInt == 1:
-            await ctx.send("The expansion has officially been cancelled.")
+            await ctx.reply("Expansion? There's an expansion?! Tell me everything!")
         elif rndInt == 2:
-            await ctx.send(f"The expansion will be out for release in just {random.randint(1, 59)} minutes!")
+            await ctx.reply(f"The expansion will be out for release in just {random.randint(1, 59)} minutes!")
         elif rndInt == 3:
-            await ctx.send("The expansion will be released whenever Half-Life 3 comes out.")
+            await ctx.reply("The expansion will be released whenever Half-Life 3 comes out.")
+        elif rndInt == 4:
+            await ctx.reply("The expansion will be released when it's done.")
+        elif rndInt == 5:
+            await ctx.reply("The expansion was gets delayed by a month every time you ask.")
         else:
-            await ctx.send(f"The expansion is planned for release in {random.randint(365, 1000)} days.")
+            msg = await ctx.reply(f"The expansion is planned for release in {random.randint(365, 1000)} days.")
+            for i in range(4):
+                await asyncio.sleep(random.randint(2, 8))
+                await msg.edit(content=f"Wait no, actually it will release in {random.randint(365, 1000)} days.")
+            await asyncio.sleep(random.randint(3, 10))
+            await msg.edit(content=f"It will release in {random.randint(365, 1000)} days... I think.")
+
+
 
 async def setup(bot):
     await bot.add_cog(FunCog(bot))
